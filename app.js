894b48b7cfe806a1313f0cead7d15cfabf6eff5a
gulinodesign-maker <gulinodesign@gmail.com>
2026-01-18 16:16:01 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 /**
  * Build: incrementa questa stringa alla prossima modifica (es. 1.001)
  */
-const BUILD_VERSION = "dDAE_2.025";
+const BUILD_VERSION = "dDAE_2.026";
 
 
 function __parseBuildVersion(v){
