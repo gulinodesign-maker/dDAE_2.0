c2d3e760d67d879cc8ad905afe9bfc74d01c7738
gulinodesign-maker <gulinodesign@gmail.com>
2026-01-20 13:53:55 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 /**
  * Build: incrementa questa stringa alla prossima modifica (es. 1.001)
  */
-const BUILD_VERSION = "dDAE_2.064";
+const BUILD_VERSION = "dDAE_2.065";
 
 // Ruoli: "user" (default) | "operatore"
 function isOperatoreSession(sess){
