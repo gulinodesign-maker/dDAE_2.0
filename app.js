029c8c2b7915bae5684a01cf76827e1cd7ceb558
gulinodesign-maker <gulinodesign@gmail.com>
2026-02-22 19:28:12 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -52,9 +52,9 @@ try{
 /* global API_BASE_URL, API_KEY */
 
 /**
- * Build: dDAE_2.214
+ * Build: dDAE_2.215
  */
-const BUILD_VERSION = "dDAE_2.214";
+const BUILD_VERSION = "dDAE_2.215";
 
 // Utility: parse importi (usato anche in guest list)
 function money(v){
